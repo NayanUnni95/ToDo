f660e3fdad64662e03e51cb118ac93a5f152fbd8
“Nayan” <nayanunni95@gmail.com>
2024-02-06 19:34:17 +0530

    change data div creation method

--- script.js
+++ script.js
@@ -11,58 +11,25 @@ let para = document.getElementById("para");
 function addTask() {
   // Function to add data
   if (input.value === "") {
-    alert("Empty Input...");
+    alert("Empty Input Field...");
   } else {
     let data = document.getElementById("mDiv"); // Main parent div
 
-    const mainDiv = document.createElement("div"); // Data div that includes the whole division
-    mainDiv.classList.add("data1");
-    data.appendChild(mainDiv);
-
-    const newDiv = document.createElement("div"); // User input div
-    newDiv.classList.add("data-div");
-    mainDiv.appendChild(newDiv);
-
-    const paraElement = document.createElement("p"); // Para element
-    paraElement.classList.add("des");
-    newDiv.appendChild(paraElement);
-    paraElement.innerText = input.value;
-
-    const icon = document.createElement("div"); // Icon div
-    icon.classList.add("icon");
-    newDiv.appendChild(icon);
-
-    const checked = document.createElement("div");
-    checked.classList.add("checked");
-    newDiv.appendChild(checked);
-
-    // const eye = document.createElement("i"); // Eye icon
-    // eye.classList.add("fa-solid");
-    // eye.classList.add("fa-eye");
-    // eye.classList.add("fa-lg");
-    // eye.classList.add("eye");
-    // checked.appendChild(eye);
-
-    const check = document.createElement("i"); // Check icon
-    check.classList.add("fa-solid");
-    check.classList.add("fa-check-circle");
-    check.classList.add("fa-lg");
-    check.classList.add("check");
-    checked.appendChild(check);
-
-    const cmp = document.createElement("h4");
-    cmp.innerText = "";
-    checked.appendChild(cmp);
-
-    const btn = document.createElement("div"); // Delete button div
-    btn.classList.add("btn");
-    mainDiv.appendChild(btn);
-    const trash = document.createElement("i"); // Delete icon
-    trash.classList.add("fa-solid");
-    trash.classList.add("fa-trash");
-    trash.classList.add("fa-lg");
-    trash.classList.add("trash");
-    btn.appendChild(trash);
+    const dataSection = `<div id="mDiv" class="main-body">
+    <div class="data1 flex flex-item flex-content">
+      <div class="data-div flex flex-item">
+        <p class="des">${input.value}</p>
+        <div class="icon"></div>
+        <div class="checked flex flex-item flex-content">
+          <i class="fa-solid fa-check-circle fa-lg check"></i>
+          <h4 class="h4"></h4>
+        </div>
+        </div>
+        <div class="btn flex flex-item flex-content"><i class="fa-solid fa-trash fa-lg trash"></i></div>
+      </div>
+    </div>`
+    data.insertAdjacentHTML("beforeend", dataSection);
+    input.value = '';
 
     btn.addEventListener("click", function () {
       // Function to delete data
@@ -74,8 +41,11 @@ function addTask() {
       btn.remove();
       trash.remove();
     });
+    const check = document.querySelector(".checked");
     check.addEventListener("click", function () {
       // Function to completed data
+      console.log(".");
+      const cmp = document.querySelector(".h4");
       cmp.innerText = "Completed";
       cmp.style.color = "#27d914";
       check.style.color = "#27d914";
@@ -83,3 +53,4 @@ function addTask() {
   }
   input.value = "";
 }
+
